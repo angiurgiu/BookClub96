f5cd6df8a8dad23675972b2d0497cffada69cc2c
Andrei Giurgiu <angiurgiu@gmail.com>
2019-04-24 17:56:37 -0700

    Added trace logger levels. Added logger to the repository.

--- BookClub96/BookClub96/Data/BookClubRepository.cs
+++ BookClub96/BookClub96/Data/BookClubRepository.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookClub96.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace BookClub96.Data
 {
     public class BookClubRepository : IBookClubRepository
     {
         private readonly BookClubContext _ctx;
+        private ILogger<BookClubRepository> _logger;
 
-        public BookClubRepository(BookClubContext ctx)
+        public BookClubRepository(BookClubContext ctx, ILogger<BookClubRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Book> GetAllBooks()
         {
-            return _ctx.Books
-                .OrderBy(b => b.Title)
-                .ToList();
+            try
+            {
+                _logger.LogInformation($"{nameof(GetAllBooks)} was called.");
+
+                return _ctx.Books
+                    .OrderBy(b => b.Title)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all books. [ex={ex}]");
+                return null;
+            }
         }
 
         public IEnumerable<Book> GetAllBooksByGenre(string genre)
